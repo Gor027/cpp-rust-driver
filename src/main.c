c1c8d6e4d00e56b2929de19c8feb53cfdbaab93a
Piotr Grabowski <piotr.grabowski@scylladb.com>
2021-10-22 17:08:48 +0200

    Implement many binding methods on CassStatement
    Implement most of the binding methods working on CassStatement. The only
    missing methods are:
    - cass_statement_bind_decimal
    - cass_statement_bind_duration - DURATION not implemented in Rust Driver
    - methods requiring implementing cpp driver data structures:
        - cass_statement_bind_user_type
        - cass_statement_bind_collection
        - cass_statement_bind_custom
        - cass_statement_bind_custom_n
        - cass_statement_bind_tuple
        - cass_statement_bind_uuid
        - cass_statement_bind_inet
    - variants of all methods with by_name, by_name_n

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 void do_prepared_query(CassSession* session, const char* query_text) {
     CassFuture* prepare_future = cass_session_prepare(session, query_text);
-    CassPrepared* prepared = cass_future_get_prepared(prepare_future);
+    const CassPrepared* prepared = cass_future_get_prepared(prepare_future);
     CassStatement* statement = cass_prepared_bind(prepared);
     CassFuture* statement_future = cass_session_execute(session, statement);
     printf("prepared query code: %d\n", cass_future_error_code(statement_future));
@@ -40,14 +40,16 @@ int main() {
     cass_future_free(connect_future);
 
     do_simple_query(session, "CREATE KEYSPACE IF NOT EXISTS ks WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}");
-    do_simple_query(session, "CREATE TABLE IF NOT EXISTS ks.t (pk int, ck int, v int, primary key (pk, ck))");
-    do_simple_query(session, "INSERT INTO ks.t(pk, ck, v) VALUES (7, 8, 9)");
-    do_prepared_query(session, "INSERT INTO ks.t(pk, ck, v) VALUES (69, 69, 69)");
+    do_simple_query(session, "DROP TABLE IF EXISTS ks.t");
+    do_simple_query(session, "CREATE TABLE IF NOT EXISTS ks.t (pk int, ck int, v int, v2 text, primary key (pk, ck))");
+    do_simple_query(session, "INSERT INTO ks.t(pk, ck, v, v2) VALUES (7, 8, 9, 'hello world')");
+    do_prepared_query(session, "INSERT INTO ks.t(pk, ck, v, v2) VALUES (69, 69, 69, 'greetings from Rust!')");
 
-    CassStatement* statement = cass_statement_new("INSERT INTO ks.t(pk, ck, v) VALUES (?, ?, ?)", 3);
+    CassStatement* statement = cass_statement_new("INSERT INTO ks.t(pk, ck, v, v2) VALUES (?, ?, ?, ?)", 4);
     cass_statement_bind_int32(statement, 0, 100);
     cass_statement_bind_int32(statement, 1, 200);
     cass_statement_bind_int32(statement, 2, 300);
+    cass_statement_bind_string(statement, 3, "We love Rust!");
 
     CassFuture* statement_future = cass_session_execute(session, statement);
     cass_future_set_callback(statement_future, print_error_cb, NULL);
